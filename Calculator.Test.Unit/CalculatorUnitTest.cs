a5bf70fc73ad49aa1db1a598d17f0db4e12817ba
Lasse Torp Jacobsen <Lasse.torp.jacobsen@post.au.dk>
2019-01-30 11:11:07 +0100

    Enitial checkin

---
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator = L01___Calculator__Unit_test_.Calculator;
+
+namespace Cal.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_ReturnsRightAmount()
+        {
+            Assert.That(calculator.Add(10,10), Is.EqualTo(20));
+        }
+        [Test]
+        public void Subtract_ReturnsRightAmount()
+        {
+            Assert.That(calculator.Subtract(10, 10), Is.EqualTo(0));
+        }
+        [Test]
+        public void Multiply_ReturnsRightAmount()
+        {
+            Assert.That(calculator.Multiply(10, 10), Is.EqualTo(100));
+        }
+        [Test]
+        public void Power_ReturnsRightAmount()
+        {
+            Assert.That(calculator.Power(10, 2), Is.EqualTo(100));
+        }
+    }
+}
---
+++ L01 - Calculator (Unit test)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01___Calculator__Unit_test_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Add({0} + {1}) = {2}", 10.00, 10.00, calculator.Add(10,10));
+            Console.WriteLine("Subtract({0} - {1}) = {2}", 10.00, 10.00, calculator.Subtract(10,10));
+            Console.WriteLine("Multiply({0} * {1}) = {2}", 10.00, 10.00, calculator.Multiply(10,10));
+            Console.WriteLine("Power({0} ^ {1}) = {2}", 10.00, 1.00, calculator.Power(10,1));
+        }
+    }
+}
