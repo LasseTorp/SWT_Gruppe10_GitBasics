f4cf4144d7ce455a9472bfdee4db355e865bd7c7
MathiasAKAchampen <201509795@post.au.dk>
2019-02-12 13:18:37 +0100

    Kommentar, hej jeppe

--- L01 - Calculator (Unit test)/Program.cs
+++ L01 - Calculator (Unit test)/Program.cs
@@ -19,6 +19,8 @@ namespace L01___Calculator__Unit_test_
             Console.WriteLine("Power({0} ^ {1}) = {2}", 10.00, 1.00, calculator.Power(10,1));
 
             Console.ReadKey();
+
+            //Hej jeppe
         }
     }
 }
